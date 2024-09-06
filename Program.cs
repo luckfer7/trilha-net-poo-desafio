389ac43803ffb2e7c6882bdb4bdd74d944c3d27a
luckfer7 <lucasferreira059@gmail.com>
2024-09-06 11:16:12 -0300

    ainda não terminei o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("994468609", "nokia2630", "123456", 64);
+nokia.InstalarAplicativo("Telegram");
+
+Iphone iphone = new Iphone ("73151895", "Iphone13", "987654", 128);
+iphone.InstalarAplicativo("instagram");
